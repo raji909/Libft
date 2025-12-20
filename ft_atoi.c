3096243ae99b12165bf11c9d9a8d0bdeb68b9a76
Adam Raji <adamraji48@gmail.com>
2025-12-20 09:53:56 +0100

    Refactor ft_atoi to use constants for signs

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,32 @@
 /*   By: adraji <adraji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 10:01:13 by adraji            #+#    #+#             */
-/*   Updated: 2025/12/15 07:12:11 by adraji           ###   ########.fr       */
+/*   Updated: 2025/12/20 09:50:13 by adraji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
 	long	n;
 	t_signe	signe;
 
 	n = 0;
-	signe = 1;
+	signe = PLUS;
 	while (*nptr && ((*nptr == ' ') || (*nptr >= 9 && *nptr <= 13)))
 		nptr++;
 	if (*nptr == '-' || *nptr == '+')
 	{
 		if (*nptr == '-')
-			signe = -1;
+			signe = MINUS;
 		nptr++;
 	}
-	while (*nptr && *nptr >= '0' && *nptr <= '9')
+	while (*nptr && ft_isdigit(*nptr))
 	{
 		if (n > LM / 10 || (n == LM / 10 && (*nptr - '0') > LM % 10))
 		{
-			if (signe == 1)
+			if (signe == PLUS)
 				return (-1);
 			return (0);
 		}
